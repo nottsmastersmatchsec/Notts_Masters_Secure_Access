f5d34f41b481a98b694b0de12c92565df31b8b0d
NottsMastersBadminton <nottsmastersmatchsec@gmail.com>
2026-06-09 21:35:02 +0100

    Update password.js

--- auth/password.js
+++ auth/password.js
@@ -1,2 +1,2 @@
 // SHA-256 hash of your chosen password
-const PASSWORD_HASH = "ff3f3be9a9c6155b7c5f6b5a40584fd68e91f99112791d3c71a0dcafe2ad279f";
+window.CAPTAINS_PASSWORD_HASH = "ff3f3be9a9c6155b7c5f6b5a40584fd68e91f99112791d3c71a0dcafe2ad279f";
